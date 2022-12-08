cf5a0ece40393f272ef59b74f55ddd8043aacf06
djarchibald <13827@coderacademy.edu.au>
2022-12-08 16:33:32 +1100

    tweaked add birthday

--- Add_birthday.py
+++ Add_birthday.py
@@ -1,17 +1,17 @@
-birthday_list = "list_of_birthdays.txt"
+birthday_file = "list_of_birthdays.txt"
 
 
-birthday_list = open("list_of_birthdays.txt", "r")
-for birthday in birthday_list.readlines():
+birthday_file = open("list_of_birthdays.txt", "r")
+for birthday in birthday_file.readlines():
     print(birthday)
 
-birthday_list = open("list_of_birthdays.txt", "a")
+birthday_file = open("list_of_birthdays.txt", "a")
 
-birthday_list.write(input("Your current list of birthdays is above. Please enter a new birthday in the format: MM/DD First Name e.g. 1018 Duncan: ") + '\n')
-birthday_list.close()
+birthday_file.write(input("Your current list of birthdays is above. Please enter a new birthday in the format: MM/DD First Name e.g. 10/18 Duncan: ") + '\n')
+birthday_file.close()
 print("Birthday added. Your list of birthdays is now:")
-birthday_list = open("list_of_birthdays.txt", "r")
+birthday_file = open("list_of_birthdays.txt", "r")
 
-for birthday in birthday_list.readlines():
+for birthday in birthday_file.readlines():
     print(birthday)
-    birthday_list.close()
+    birthday_file.close()
