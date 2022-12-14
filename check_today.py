1f8b083fc936ae30c1e906df834fd0f92a68272f
djarchibald <13827@coderacademy.edu.au>
2022-12-15 10:51:00 +1100

    tidied up chekcbirthdays function. added some explanatory comments

--- check_today.py
+++ check_today.py
@@ -8,6 +8,7 @@ def checkbirthdays():
     birthdays = open(birthday_file, 'r')
     today = time.strftime('%m/%d')
     flag = 0
+    # iterate through file, comparing date to current date
     for line in birthdays:
         if today in line:
             line = line.split(' ')
@@ -16,7 +17,7 @@ def checkbirthdays():
             print("Remember to wish: [red]" + line[1] + ' ' + line[2] + ' ' "[/red]a very happy birthday!")
     
     if flag == 0:
-   
+        # message for if no birthdays matching today
         print("[italic green]Don't worry, you didn't forget anyone. There are no birthdays today.")
   
 if __name__ == '__main__':
